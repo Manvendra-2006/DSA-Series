3869bce9b937715dda24ee6d585ce65efb8f325d
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-03-24 01:00:48 +0530

    Add Pattern Printing

---
+++ PatternPrinting/3rdProgram.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            cout<<i<<" ";
+        }
+        cout<<"\n"<<endl;
+    }
+    return 0;
+}
